ac4c038042900225d0932a1340c6724cde107f80
JaktensTid <jaktenstid1@gmail.com>
2017-01-15 21:46:15 +0300

    Last modifications related to priority

--- OCRorders.py
+++ OCRorders.py
@@ -5,26 +5,33 @@ import csv
 import os
 import json
 
-priorities = ['application', 'exhibit', 'interested part']
-total_ocred = 0
+priorities = ['parties', 'application', 'exhibit']
 
 def get_collection():
     client = MongoClient(os.environ['MONGODB_URI'])
     db = client['heroku_zsl3pl6l']
     return db.orders
 
+
 def make_url(doc_number, doc_url):
     return 'http://35.165.174.118:8080/plss?docNo=' + doc_number + '&url=%27http://ogccweblink.state.co.us/' + doc_url + '%27'
 
 
 def handler(d):
-    print('Thread is fetching ' + d['doc'])
+    print('Thread is fetching ' + d['cause'] + '-' + d['order'])
     collection = get_collection()
+
+    def insert(res):
+        if res['normAddress']:
+            print('Got something in ' + d['cause'] + '-' + d['order'])
+            collection.insert_one(res)
+            return 1
+        return 0
+
     def ocr(row, times=1):
-        global total_ocred
         doc_url = row[2].replace("['", '').replace("']", '')
         if doc_url == 'DownloadDocument.aspx?DocumentId=':
-            return
+            return {'normAddress' : []}
         doc_number = row[0]
         try:
             response = requests.get(make_url(doc_number, doc_url))
@@ -32,9 +39,7 @@ def handler(d):
             if d['ocrFailed'] == 'true':
                 print('Fetching ' + doc_number + ' - ' + doc_url + ' again')
                 if times < 2:
-                    ocr(row,times=times + 1)
-                return
-            total_ocred += 1
+                    return ocr(row,times=times + 1)
             d['causeNum'] = row[0]
             d['orderNum'] = row[1]
             d['docLink'] = row[2].replace("['", '').replace("']", '')
@@ -42,22 +47,34 @@ def handler(d):
             d['docDate'] = row[4]
             d['description'] = row[5]
             d['docUrl'] = d['docLink']
-            collection.insert_one(d)
-            print('Fetched ' + doc_number + ' - ' + doc_url + '. Total fetched: ' + str(total_ocred))
+            return d
         except Exception as e:
             if str(e) == 'ocrFailed':
                if times < 2:
-                   ocr(row, times=times+1)
+                   return ocr(row, times=times+1)
             print(str(e) + ' - ' + doc_url)
-    for row in d['rows']:
-        ocr(row)
+    for row in d[priorities[0]]:
+        res = ocr(row)
+        flag = insert(res)
+        if flag:
+            return
+
+    for row in d[priorities[1]]:
+        res = ocr(row)
+        flag = insert(res)
+        if flag:
+            return
+
+    for row in d[priorities[2]]:
+        res = ocr(row)
+        flag = insert(res)
+        if flag:
+            return
+
+    print('Nothing fetched in ' + d['cause'] + '-' + d['order'])
 
 
 def ocr_all():
-    #from datetime import datetime
-    #today = datetime.today()
-    #if not (today.day == 14 and today.year == 2017 and today.month == 1):
-    #    return
     def check_priority(name):
         for priority in priorities:
             if priority in name:
@@ -68,11 +85,12 @@ def ocr_all():
             reader = csv.reader(file, delimiter=',', quotechar='"')
             rows = [row for row in reader if check_priority(row[3])]
         grouped = []
-        for docno in list(set([row[0] for row in rows])):
-            d = {'doc' : docno, 'rows' : []}
-            for row in rows:
-                if row[0] == docno:
-                    d['rows'].append(row)
+        for order in list(set([(row[0],row[1]) for row in rows])):
+            d = { 'cause' : order[0], 'order' : order[1] }
+            doc_rows = [row for row in rows if row[0] == order[0] and row[1] == order[1]]
+            d[priorities[0]] = [row for row in doc_rows if priorities[0] in row[3].lower()]
+            d[priorities[1]] = [row for row in doc_rows if priorities[1] in row[3].lower()]
+            d[priorities[2]] = [row for row in doc_rows if priorities[2] in row[3].lower()]
             grouped.append(d)
         with Pool(2) as p:
             p.map(handler, grouped)
