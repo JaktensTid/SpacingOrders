038f4b7814f03afdcdd27b52ab06795b980c809d
JaktensTid <jaktenstid1@gmail.com>
2017-01-17 19:55:31 +0300

    Fixed tabs error

--- OCRorders.py
+++ OCRorders.py
@@ -81,7 +81,7 @@ def handler(d):
 
     if last_scraped:
         print('Inserting emtpy doc for ' + d['cause'] + '-' + d['order'])
-    	collection.insert(last_scraped)
+        collection.insert(last_scraped)
 
     print('Nothing fetched in ' + d['cause'] + '-' + d['order'])
 
